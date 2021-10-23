49832a3e316d8965302cf8a28f60337bef8db8fd
Mara <mara@the-witch.ru>
2021-10-23 05:14:26 +0300

    Worked

--- app/RingBuffer/RingBuffer.php
+++ app/RingBuffer/RingBuffer.php
@@ -6,26 +6,26 @@ use App\Contracts\DataStructure\Buffer;
 
 class RingBuffer implements Buffer {
     protected array $stack;
-    protected int $n;
 
     function __construct(int $itemsNumber) {
-        $this->n = $itemsNumber;
         $this->stack = array_fill(0, $itemsNumber, null);
     }
 
     function push(int $item): Buffer {
-        $key = key($this->stack);
+        $this->stack[key($this->stack)] = $item;
 
-        if ($key > ($this->n-1))
-            reset($this->stack);
-
-        $this->stack[$key] = $item;
-        next($this->stack);
+        key($this->stack) < count($this->stack)-1
+            ? next($this->stack)
+            : reset($this->stack);
 
         return $this;
     }
 
     function shift(): int|null {
         return array_shift($this->stack);
+    }
+
+    function stack(): array {
+        return array_filter($this->stack);
     }
 }
